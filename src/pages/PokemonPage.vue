487a943ce2edd261a0ae7ccb917fa016998eacf3
Roberto Núñez <robertonc1599@gmail.com>
2024-01-09 21:29:03 -0500

    Showing possible options for pokemon

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>¿Quién es este Pokémon?</h1>
+  <PokemonImage :pokemonId="4" :showPokemon="true" />
+  <PokemonOptions :pokemons="pokemonArr" />
+</template>
+
+<script>
+import PokemonImage from "@/components/PokemonImage.vue";
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptions.js";
+
+export default {
+  name: "PokemonPage",
+  components: {
+    PokemonImage,
+    PokemonOptions,
+  },
+  data() {
+    return {
+      pokemonArr: [],
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions();
+      console.log(this.pokemonArr);
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
+};
+</script>
